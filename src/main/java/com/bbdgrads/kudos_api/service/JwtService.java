f75e87fc9c4d27a7b20632e93b5a716397dc8e5c
Kyle Wilkins <Kyle.Wilkins@bbd.co.za>
2025-03-16 17:25:32 +0200

    Create auth flow with cli and secured api with jwt

---
+++ src/main/java/com/bbdgrads/kudos_api/service/JwtService.java
@@ -0,0 +1,68 @@
+package com.bbdgrads.kudos_api.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+import com.google.auth.oauth2.TokenVerifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${google.client.id}")
+    private String CLIENT_ID;
+    @Value("${kudos.api.secret}")
+    private String API_SECRET;
+    private final String KUDOS_ISSUER = "kudos-api";
+
+    public boolean verifyGoogleJwt(String jwtToken){
+        try {
+            String GOOGLE_ISSUER = "https://accounts.google.com";
+            TokenVerifier verifier = TokenVerifier.newBuilder()
+                    .setAudience(CLIENT_ID)
+                    .setIssuer(GOOGLE_ISSUER)
+                    .build();
+
+            verifier.verify(jwtToken);
+
+            DecodedJWT decodedJWT = JWT.decode(jwtToken);
+            // Could potentially get user info here
+//            System.out.println("User ID: " + decodedJWT.getSubject());
+//            System.out.println("Email: " + decodedJWT.getClaim("email").asString());
+            return true;
+
+        } catch (JWTVerificationException | TokenVerifier.VerificationException e){
+            System.err.println("JWT Verification failed: " + e.getMessage());
+            return false;
+        }
+    }
+
+    public String generateApiJwt(String userId){
+        String KUDOS_ISSUER = "kudos-api";
+        long EXPIRATION_TIME = 86400000;
+        return JWT.create()
+                .withSubject(userId)
+                .withIssuer(KUDOS_ISSUER)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(Algorithm.HMAC256(API_SECRET));
+    }
+
+    public DecodedJWT verifyApiJwt(String apiJwt){
+        try{
+            JWTVerifier verifier = JWT.require(Algorithm.HMAC256(API_SECRET))
+                    .withIssuer(KUDOS_ISSUER)
+                    .build();
+            return  verifier.verify(apiJwt);
+        } catch (JWTVerificationException e){
+            System.err.println("JWT Verification failed: " + e.getMessage());
+            return null;
+        }
+
+    }
+}
